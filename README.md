4c8c48a92762d64ff46589af8cf7b69de70bb290
jayone <imdff0803@gmail.com>
2021-05-23 21:52:01 +0900

    React - Elements

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # React Express
 - Reference: https://www.react.express/
+
+## Links
+- 1. [Setup and Tools](https://www.react.express/environment/setup_and_tools)
+- 2. [JSX](https://www.react.express/jsx)
+- 3. [React](https://www.react.express/react)
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const myElement = (
+  <div foo="bar">
+    <button>Test</button>
+    <span>Hello</span>
+  </div>
+)
+
 function App() {
   const style = {
     padding: '40px',
     textAlign: 'center',
   }
+
+  console.log(JSON.parse(JSON.stringify(myElement)))
 
   return <div style={style}>Welcome to React!</div>
 }
