e29cfc38b9e6194164c374d8c357047de1e4dd7e
jayone <imdff0803@gmail.com>
2021-05-24 02:00:38 +0900

    React - Styling - Inline Styles

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+function randomColor() {
+  const color = `#${Math.random().toString(16).substr(-6)}`;
+  console.log(color);
+  return color;
+}
 
-function MyComponent(props) {
+function Card({ color, children }) {
   return (
-    <div style={{padding: '30px', backgroundColor: 'lightblue'}}>
-      <button>{props.text}</button>
+    <div
+      style={{
+        padding: "20px",
+        textAlign: "center",
+        color: "white",
+        backgroundColor: color,
+      }}
+    >
+      {children}
     </div>
-  )
+  );
 }
 
 function App() {
+  const [color, setColor] = useState("skyblue");
+
   const style = {
-    padding: '40px',
-    textAlign: 'center',
-  }
+    padding: "40px",
+    textAlign: "center",
+  };
 
   return (
     <>
       <div style={style}>Welcome to React!</div>
-      <MyComponent text="button1" />
-      <MyComponent text="button2" />
+      <div style={{ padding: "20px" }}>
+        <Card color={color}>
+          <input
+            type={"button"}
+            value={"Randomize Color"}
+            onClick={() => setColor(randomColor())}
+          />
+        </Card>
+      </div>
     </>
-  )
+  );
 }
 
-ReactDOM.render(<App />, document.querySelector("#app"))
+ReactDOM.render(<App />, document.querySelector("#app"));
