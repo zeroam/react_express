4351f1bcff8056302fdbac4b695102851c0af9f7
jayone <imdff0803@gmail.com>
2021-05-26 00:34:23 +0900

    React - User Input

--- index.js
+++ index.js
@@ -1,42 +1,35 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-// Custom Components and Events
-function CounterButton({ style, title, onPress }) {
+function TextInput() {
+  const [state, setState] = useState("");
+
   return (
-    <button style={style} onClick={onPress}>
-      {title}
-    </button>
+    <div>
+      <label htmlFor={"my-input"}>Enter text: </label>
+      <input
+        id={"my-input"}
+        type={"text"}
+        value={state}
+        placeholder={"Type here"}
+        onChange={(event) => {
+          setState(event.target.value);
+        }}
+      />
+      <br />
+      <br />
+      You entered: {state}
+    </div>
   );
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const style = {
-    display: "block",
-    padding: "40px",
-    textAlign: "center",
-    marginLeft: "auto",
-    marginRight: "auto"
-  };
-
   return (
     <>
-      <div style={style}>Welcome to React!</div>
-      <button
-        style={style}
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Click HERE to increment: {count}
-      </button>
-      <CounterButton
-        style={style}
-        title={`Click HERE to increment: ${count}`}
-        onPress={() => setCount(count + 1)}
-      />
+      <div style={{ padding: "40px", textAlign: "center" }}>
+        Welcome to React!
+      </div>
+      <TextInput />
     </>
   );
 }
