f36ac4f980cc35668ad0698856a8ad7d1df0d0bc
jayone <imdff0803@gmail.com>
2021-05-26 00:28:29 +0900

    React - Events

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import styled from 'styled-components'
 
-function randomColor() {
-  const color = `#${Math.random().toString(16).substr(-6)}`;
-  console.log(color);
-  return color;
+// Custom Components and Events
+function CounterButton({ style, title, onPress }) {
+  return (
+    <button style={style} onClick={onPress}>
+      {title}
+    </button>
+  );
 }
 
-const Card = styled.div`
-  padding: 20px;
-  text-align: center;
-  color: white;
-  background-color: ${props => props.color};
-`
-
-const Container = styled.div`
-  padding: 20px;
-`
-
 function App() {
-  const [color, setColor] = useState("skyblue");
+  const [count, setCount] = useState(0);
 
   const style = {
+    display: "block",
     padding: "40px",
     textAlign: "center",
+    marginLeft: "auto",
+    marginRight: "auto"
   };
 
   return (
     <>
       <div style={style}>Welcome to React!</div>
-      <Container>
-        <Card color={color}>
-          <input
-            type={'button'}
-            value={'Randomize Color'}
-            onClick={() => setColor(randomColor())}
-          />
-        </Card>
-      </Container>
+      <button
+        style={style}
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click HERE to increment: {count}
+      </button>
+      <CounterButton
+        style={style}
+        title={`Click HERE to increment: ${count}`}
+        onPress={() => setCount(count + 1)}
+      />
     </>
   );
 }
