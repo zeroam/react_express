ba291670fc65ef7b83f7c141cfa19ba3f159eb8f
jayone <imdff0803@gmail.com>
2021-05-27 01:33:41 +0900

    Hooks - useReducer

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
 
-const randomDiceRoll = () => {
-  return Math.floor(Math.random() * 6) + 1;
+const types = {
+  PET: "PET",
+  COLOR: "COLOR",
 };
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case types.PET:
+      return { ...state, pet: action.value };
+    case types.COLOR:
+      return { ...state, color: action.value };
+  }
+};
+
+const initialState = {
+  color: 'black',
+  pet: 'cat',
+}
+
 function App() {
-  const [diceRolls, setDiceRolls] = useState([1, 2, 3]);
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <>
       <div style={{ padding: "40px", textAlign: "center" }}>
         Welcome to React!
       </div>
-      <button
-        onClick={() => {
-          setDiceRolls([...diceRolls, randomDiceRoll()]);
-        }}
-      >
-        Roll dice
-      </button>
-      <ul>
-        {diceRolls.map((diceRoll, index) => {
-          return <li key={index}>{diceRoll}</li>
-        })}
-      </ul>
+      <div>
+        <label>Choose a color and a pet: </label>
+        <br />
+        <select
+          value={state.color}
+          onChange={event=> {
+            dispatch({ type: types.COLOR, value: event.target.value})
+          }}
+        >
+          <option value="black">Black</option>
+          <option value="pink">Pink</option>
+          <option value="blue">Blue</option>
+        </select>
+        <select
+          value={state.pet}
+          onChange={event => {
+            dispatch({ type: types.PET, value: event.target.value})
+          }}
+        >
+          <option value="cat">Cat</option>
+          <option value="dog">Dog</option>
+          <option value="mouse">Mouse</option>
+        </select>
+        <br />
+        <br />
+        You choose a {state.color} {state.pet}
+      </div>
     </>
   );
 }
