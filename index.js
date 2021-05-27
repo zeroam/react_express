eb796322b8d9132da7a53680148c064150b6f9fe
jayone <imdff0803@gmail.com>
2021-05-28 00:25:32 +0900

    Hooks - useContext

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useContext, createContext } from "react";
 import ReactDOM from "react-dom";
 
+const ThemeConext = createContext();
+
+function Title() {
+  const theme = useContext(ThemeConext);
+
+  const style = {
+    background: theme.primary,
+    color: theme.text,
+  }
+
+  return <h1 style={style}>Title</h1>
+}
+
+function Nested() {
+  return <Title />
+}
+
+function NestedTwice() {
+  return <Nested />
+}
+
 function App() {
-  const intervalRef = useRef();
-  const [count, setCount] = useState(0);
-
-  const canvasRef = useRef();
-
-  console.log("rendering");
-
-  useEffect(() => {
-    /* mutable value example */
-    intervalRef.current = setInterval(() => setCount(count => count + 1), 1000);
-    // variable = setInterval(() => setCount(count => count + 1), 1000);
-
-    /* DOM example */
-    const canvas = canvasRef.current
-    const context = canvas.getContext("2d");
-
-    context.fillStyle = 'rgba(59, 108, 212, 0.5)'
-    context.fillRect(10, 10, 50, 50)
-
-    context.fillStyle = 'rgba(59, 108, 212)'
-    context.fillRect(30, 30, 50, 50)
-  }, []);
+  const theme = {
+    primary: 'dodgerblue',
+    text: 'white',
+  }
 
   return (
     <>
       <div style={{ padding: "40px", textAlign: "center" }}>
         Welcome to React!
       </div>
-      <div style={{ fontSize: 120 }}>{count}</div>
-      <button
-        onClick={() => {
-          clearInterval(intervalRef.current)
-          // clearInterval(variable)
-        }}
-      >
-        Stop
-      </button>
-      <canvas ref={canvasRef} />
+      <ThemeConext.Provider value={theme}>
+        <NestedTwice />
+      </ThemeConext.Provider>
     </>
   );
 }
